bd58d58544f514ecf999c3a85a31b1d87cef1deb
mstoecklein <mstoecklein@xp.live>
2023-01-18 03:48:02 +0100

    OCD

--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
 import { Interpreter, InterpreterOptions } from "./src/Interpreter.ts";
 import { ASTNode } from "./src/Lexer.ts";
 
-let interpreter!: Interpreter;
+export * from "./src/Interpreter.ts";
+export * from "./src/Lexer.ts";
+export * from "./src/BuiltInHelpers.ts";
 
+let interpreter!: Interpreter;
 function getInstance(options: Partial<InterpreterOptions> = {}) {
   if (!interpreter) {
     interpreter = new Interpreter(options);
   }
   return interpreter;
 }
-
-export * from "./src/Interpreter.ts";
-export * from "./src/Lexer.ts";
-export * from "./src/BuiltInHelpers.ts";
 
 export function tokenize(
   template: string,
