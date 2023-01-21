c30ea775317610610668af6d7716fa756d7a955b
mstoecklein <mstoecklein@xp.live>
2023-01-21 01:49:18 +0100

    do some shenanigans to play nice with view_engine

--- src/Adapter.test.ts
+++ src/Adapter.test.ts
@@ -1,13 +1,107 @@
+import { stub } from "https://deno.land/std@0.165.0/testing/mock.ts";
 import { assertEquals } from "https://deno.land/std@0.131.0/testing/asserts.ts";
-import { bartEngine } from "./Adapter.ts";
+import { bartEngine, setOptions } from "./Adapter.ts";
+
+stub(Deno, "readDirSync", () => [
+  {
+    name: "header.html",
+    isFile: true,
+    isDirectory: false,
+    isSymlink: false,
+  },
+  {
+    name: "footer.html",
+    isFile: true,
+    isDirectory: false,
+    isSymlink: false,
+  },
+]);
+
+stub(Deno, "readTextFileSync", (url: string | URL) => {
+  const path = url instanceof URL ? url.pathname : url;
+  if (path === "/partials/header.html") {
+    return "<h1>Hello, {{name}}</h1>";
+  } else if (path === "/partials/footer.html") {
+    return "<footer>&copy; 2023 by me</footer>";
+  }
+  return "";
+});
+
+stub(Deno, "readTextFile", (url: string | URL) => {
+  const path = url instanceof URL ? url.pathname : url;
+  let value = "";
+  if (path === "/layouts/default.html") {
+    value = "{{{content}}}";
+  } else if (path === "/layouts/default2.html") {
+    value = "{{> header}}{{{content}}}";
+  }
+  return Promise.resolve(value);
+});
 
 Deno.test(
   "Testing bartEngine()",
   async () => {
-    const template = `<h1>{{data.name}}</h1>`;
-
-    const actual = await bartEngine(template, { data: { name: "John" } });
-    const expect = `<h1>John</h1>`;
-    assertEquals(actual, expect);
+    const engine = bartEngine({
+      partialPath: "/partials",
+      layoutPath: "/layouts",
+      layout: "default.html",
+      extName: ".html",
+    });
+    const result = await engine(
+      "{{> header}}<p>{{subheader}}</p>{{> footer}}",
+      {
+        title: "Test",
+        subheader: "This is a test",
+        name: "John",
+      },
+    );
+    assertEquals(
+      result,
+      "<h1>Hello, John</h1><p>This is a test</p><footer>&copy; 2023 by me</footer>",
+    );
   },
 );
+
+Deno.test("Testing bartEngine() with no layout", async () => {
+  const engine = bartEngine({
+    partialPath: "/partials",
+    extName: ".html",
+  });
+  const result = await engine("{{> header}}", {
+    name: "John",
+  });
+  assertEquals(result, "<h1>Hello, John</h1>");
+});
+
+Deno.test("Testing bartEngine() with local options", async () => {
+  const engine = bartEngine({
+    partialPath: "/partials",
+    layoutPath: "/layouts",
+    layout: "default.html",
+    extName: ".html",
+  });
+
+  // local options should override global options
+  setOptions({
+    layout: "default2.html",
+  });
+
+  const result = await engine("<p>test</p>", {
+    name: "John",
+  });
+
+  assertEquals(
+    result,
+    "<h1>Hello, John</h1><p>test</p>",
+  );
+
+  // global options should be unchanged after local options are used
+  const result2 = await engine("<p>{{name}} is not in the header</p>", {
+    name: "Jane",
+  });
+
+  assertEquals(
+    result2,
+    "<p>Jane is not in the header</p>",
+  );
+});
--- src/Adapter.ts
+++ src/Adapter.ts
@@ -1,26 +1,69 @@
-// deno-lint-ignore-file ban-types no-unused-vars
 import type {
   Engine,
   ViewConfig,
 } from "https://deno.land/x/view_engine@v10.6.0/lib/viewEngine.type.ts";
+import { basename } from "https://deno.land/std@0.151.0/path/mod.ts";
 import { Bart } from "./Bart.ts";
 
-export const bart = new Bart();
+export interface BartEngineConfig {
+  partialPath: string;
+  layoutPath: string;
+  layout: string;
+  extName: string;
+}
 
-export const bartEngine: Engine = (
-  template: string,
-  data: object = {},
-  config: ViewConfig = {},
-  filename = "",
-) => {
-  return new Promise<string>((resolve, reject) => {
-    try {
-      const result = bart.compile(template)(
-        data as Record<string, unknown>,
-      ) as string;
-      resolve(result);
-    } catch (e) {
-      reject(e);
-    }
-  });
+const globalConfig: BartEngineConfig = {
+  partialPath: "",
+  layoutPath: "",
+  layout: "",
+  extName: ".html",
 };
+let localConfig: Partial<BartEngineConfig> = {};
+
+export function setOptions(options: Partial<BartEngineConfig>): void {
+  localConfig = options;
+}
+
+export function bartEngine(options: Partial<BartEngineConfig>): Engine {
+  const bart = new Bart();
+  globalConfig.extName = options.extName ?? globalConfig.extName;
+  globalConfig.layout = options.layout ?? globalConfig.layout;
+  globalConfig.layoutPath = options.layoutPath ?? globalConfig.layoutPath;
+  globalConfig.partialPath = options.partialPath ?? globalConfig.partialPath;
+
+  // read directory and register partials
+  if (options.partialPath) {
+    const partials = Deno.readDirSync(options.partialPath);
+    const templates: Record<string, string> = {};
+    for (const partial of partials) {
+      if (!partial.isFile) {
+        continue;
+      }
+      const name = basename(partial.name, options.extName);
+      const template = Deno.readTextFileSync(
+        `${options.partialPath}/${partial.name}`,
+      );
+      templates[name] = template;
+    }
+    bart.registerPartials(templates);
+  }
+
+  return async (
+    template: string,
+    data: object = {},
+  ) => {
+    const options = { ...globalConfig, ...localConfig };
+    const content = bart.compile(template)(data as Record<string, unknown>);
+    localConfig = {};
+
+    if (options.layout) {
+      const layout = options.layoutPath
+        ? `${options.layoutPath}/${options.layout}`
+        : options.layout;
+      const layoutTmpl = await Deno.readTextFile(layout);
+      const layoutData = { ...data, content };
+      return bart.compile(layoutTmpl)(layoutData as Record<string, unknown>);
+    }
+    return content;
+  };
+}
